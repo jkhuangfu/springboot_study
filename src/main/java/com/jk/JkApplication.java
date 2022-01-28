97f4925ac8618cae8e815668d1eec405c607ecf4
jkhuangfu <huangfujiankang@gmail.com>
2022-01-28 10:18:18 +0800

    -update:删除日志

--- src/main/java/com/jk/JkApplication.java
+++ src/main/java/com/jk/JkApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@MapperScan("com.jk.dao") //扫描dao接口所在包同时将dao所有接口在工厂中创建对象
 public class JkApplication {
   public static void main(String[] args) {
-    System.out.println("args = " + args);
     SpringApplication.run(JkApplication.class, args);
   }
-
 }
